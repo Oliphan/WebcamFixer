fae1cab913ab289e061ec70072a54a70950e21a5
Oliphan <bormail68@gmail.com>
2020-06-14 02:47:36 +0100

    Webcam Fixer Basic Working Version

---
+++ WebcamFixer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using AForge.Video.DirectShow;
+
+namespace WebcamFixer
+{
+    class WebcamFixer
+    {
+        static void Main(string[] args)
+        {
+            FilterInfoCollection filterCol = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            foreach (FilterInfo filterInfo in filterCol)
+            {
+                VideoCaptureDevice tmp = new VideoCaptureDevice(filterInfo.MonikerString);
+                try
+                {
+                    tmp.SetCameraProperty((CameraControlProperty)(19), 0, CameraControlFlags.None);
+                }
+                catch { }
+            }
+        }
+    }
+}
